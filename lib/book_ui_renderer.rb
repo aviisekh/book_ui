a6e7e0d84bacf6f05beed32eda7786a8bed6aebc
Abhishek Bhatta <aviisekh@Abhisheks-MacBook-Pro.local>
2024-11-12 10:50:04 +0545

    Initial Commit

---
+++ lib/book_ui_renderer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "book_ui_renderer/version"
+
+module BookUiRenderer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/book_ui_renderer/engine.rb
@@ -0,0 +1,6 @@
+# lib/book_ui_renderer/engine.rb
+module BookUiRenderer
+  class Engine < ::Rails::Engine
+    isolate_namespace BookUiRenderer
+  end
+end
